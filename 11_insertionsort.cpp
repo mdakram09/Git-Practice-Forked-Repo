5b43d494c3e19da6764cba372186f1772eba5dea
MR-SINIST3R <88620054+MR-SINIST3R@users.noreply.github.com>
2022-10-31 22:57:15 +0530

    insertionsort fixed

--- 11_insertionsort.cpp
+++ 11_insertionsort.cpp
@@ -14,7 +14,7 @@ void insertion_sort(int arr[], int n){
     int temp,j;
     for(int i=1; i<n; i++){//comparing 2nd to nth element with the elements before them
         temp = arr[i];
-        j=i-1;
+        j = i-1;
         while(j>=0 && arr[j]>temp){
             arr[j+1]=arr[j];
             j--;
